b0c6b66a7c87bf4cdd52019ecb6daccf3f59be62
Codere-Israel <uigyhiojppoji@@@jhfvhj1234>
2024-11-21 07:54:43 +0200

    temp 5

---
+++ src/constants/imagePaths.js
@@ -0,0 +1,18 @@
+export const IMAGES = {
+  HERO: {
+    BACKGROUND: '/images/hero/hero-bg.webp',
+    MOBILE: '/images/hero/hero-mobile.jpg',
+  },
+  MENU: {
+    FISH: '/images/menu/fish.webp',
+    CHIPS: '/images/menu/chips.jpg',
+    SIDES: '/images/menu/sides.jpg',
+    DRINKS: '/images/menu/drinks.jpg'
+  },
+  GALLERY: {
+    RESTAURANT: '/images/gallery/restaurant.jpg',
+  },
+  LOCATION: {
+    JAFFA_PORT: '/images/location/jaffa-port.jpg',
+  },
+}; 
